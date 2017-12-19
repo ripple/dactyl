fbcf56f05bc7c835f1c7ac48154b5d2f279ce31d
EternalKenshi <33845125+EternalKenshi@users.noreply.github.com>
2017-12-19 15:56:33 -0800

    Added P2 Dactyl tests
    Includes adjustments to test_build_from_target and the addition of test_preprocessing.json for one of the P2 cases.

--- tests/testdactyl.py
+++ tests/testdactyl.py
@@ -14,7 +14,9 @@ class TestDactyl(unittest.TestCase):
 	        remove_tree("out")
         except FileNotFoundError:
             print("No out/ dir to remove")
-	
+
+    #P1 tests defined below
+
     def test_list(self):
         subprocess.check_output(["dactyl_build","-l"])
 	
@@ -34,7 +36,7 @@ class TestDactyl(unittest.TestCase):
         assert os.path.isfile("out/index.html")
         assert os.path.isfile("out/gfm-compat.html")
 
-    def test_generate_html_only_one_page(self):
+    def test_generate_only_one_page_using_html_extension(self):
         subprocess.check_call(["dactyl_build","--only","gfm-compat.html"])
         assert os.path.isfile("out/gfm-compat.html")
 
@@ -56,6 +58,53 @@ class TestDactyl(unittest.TestCase):
 
     def test_build_from_target(self):
         subprocess.check_call(["dactyl_build","-t","filterdemos"])
+        assert os.path.isfile("out/filter-examples-callouts.html")
+        assert os.path.isfile("out/filter-examples-xrefs.html")
+        assert os.path.isfile("out/filter-examples-buttonize.html")
+        assert os.path.isfile("out/filter-examples-badges.html")
+        assert os.path.isfile("out/filter-examples-include_code.html")
+        assert os.path.isfile("out/filter-examples-multicode_tabs.html")
+
+    #P2 tests defined below
+
+    def test_generate_markdown(self):
+        subprocess.check_call(["dactyl_build","--md"])
+        assert os.path.isfile("out/includes.md")
+        assert os.path.isfile("out/conditionals.md")
+        assert os.path.isfile("out/lists-and-codeblocks.md")
+        assert os.path.isfile("out/gfm-compat.md")
+        assert os.path.isfile("out/filter-examples/callouts.md")
+        assert os.path.isfile("out/filter-examples/xrefs.md")
+        assert os.path.isfile("out/filter-examples/buttonize.md")
+        assert os.path.isfile("out/filter-examples/badges.md")
+        assert os.path.isfile("out/filter-examples/include_code.md")
+        assert os.path.isfile("out/filter-examples/multicode_tabs.md")
+	
+    def test_generate_markdown_only_one_page(self):
+        subprocess.check_call(["dactyl_build","-c","dactyl-config.yml","--only","gfm-compat.md","--md"])
+        assert os.path.isfile("out/gfm-compat.md")
+
+    def test_generate_only_one_page_using_md_extension(self):
+        subprocess.check_call(["dactyl_build","--only","gfm-compat.md"])
+        assert os.path.isfile("out/gfm-compat.html")
+		
+    def test_preprocessing_command_line(self):
+        subprocess.check_call(["dactyl_build","--vars","{\"target\":\"filterdemos\"}"])
+        assert os.path.isfile("out/filter-examples-callouts.html")
+        assert os.path.isfile("out/filter-examples-xrefs.html")
+        assert os.path.isfile("out/filter-examples-buttonize.html")
+        assert os.path.isfile("out/filter-examples-badges.html")
+        assert os.path.isfile("out/filter-examples-include_code.html")
+        assert os.path.isfile("out/filter-examples-multicode_tabs.html")
+
+    def test_preprocessing_json(self):
+        subprocess.check_call(["dactyl_build","--vars","../tests/test_preprocessing.json"])
+        assert os.path.isfile("out/filter-examples-callouts.html")
+        assert os.path.isfile("out/filter-examples-xrefs.html")
+        assert os.path.isfile("out/filter-examples-buttonize.html")
+        assert os.path.isfile("out/filter-examples-badges.html")
+        assert os.path.isfile("out/filter-examples-include_code.html")
+        assert os.path.isfile("out/filter-examples-multicode_tabs.html")
 
 if __name__ == '__main__':
     unittest.main()
