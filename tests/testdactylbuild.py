5ad702c8ba33813b8cbf54cb132ee22a96a2c0c5
EternalKenshi <33845125+EternalKenshi@users.noreply.github.com>
2018-01-16 17:46:54 -0800

    Added test case to testdactylbuild.py
    Added P1 test case for get_pages

--- tests/testdactylbuild.py
+++ tests/testdactylbuild.py
@@ -5,11 +5,12 @@ import unittest
 from dactyl import dactyl_build
 
 class TestDactylBuild(unittest.TestCase):
-    dactyl_build.config = {"pdf_filename_fields": ["display_name"], 
+    dactyl_build.config = {"content_path": "test_files",
+                           "pdf_filename_fields": ["display_name"], 
 	                       "pdf_filename_separator": "\'-\'",
                            "default_filters": ["this_is_a_default"],						   
                            "targets": [{"name": "test_target"}, {"name": "filters_target", "filters": ["target_filter"]}, {"name": "conditionals", "display_name": "Conditional Text Target"}],
-                           "pages": [{"name": "filters_page", "filters": ["page_filter"]}]}
+                           "pages": [{"name": "filters_page", "filters": ["page_filter"]}, {"name": "test_page", "html": "test.html", "targets": ["test_target"]}, {"name": "md_test"}]}
 
     def test_default_pdf_name(self):
         test_result = dactyl_build.default_pdf_name("conditionals")
@@ -19,11 +20,14 @@ class TestDactylBuild(unittest.TestCase):
         assert dactyl_build.get_target(None) == {"name": "test_target"}
         assert dactyl_build.get_target("conditionals") == {"name": "conditionals", "display_name": "Conditional Text Target"}
         assert dactyl_build.get_target({"name": "runtime_target"}) == {"name": "runtime_target"}
+
+    def test_get_pages(self):
+        assert dactyl_build.get_pages(dactyl_build.get_target("test_target"), False) == [{"name": "test_page", "html": "test.html", "targets": ["test_target"]}]
     
     def test_get_filters_for_page(self):
         assert dactyl_build.get_filters_for_page(dactyl_build.config["pages"][0], dactyl_build.get_target("filters_target")) == {"this_is_a_default", "page_filter", "target_filter"}
     
-    
+
 
 if __name__ == '__main__':
     unittest.main()
