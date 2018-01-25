160993317549c7f70fdb2a30e46e3d4a66977a1c
EternalKenshi <33845125+EternalKenshi@users.noreply.github.com>
2018-01-25 14:48:38 -0800

    Added ElasticSearch integration tests

--- tests/testdactyl.py
+++ tests/testdactyl.py
@@ -11,7 +11,7 @@ from pathlib import Path
 class TestDactyl(unittest.TestCase):
 
     def setUp(self):
-        #Before each test is run, remonve the existing files in out subdirectory.
+        #Before each test is run, remove the existing files in out subdirectory.
         try:
 	        remove_tree("out")
         except FileNotFoundError:
@@ -68,9 +68,23 @@ class TestDactyl(unittest.TestCase):
         assert os.path.isfile("out/filter-examples-multicode_tabs.html")
 
     #P2 tests defined below
+    def test_elastic_search_default(self):
+        subprocess.check_call(["dactyl_build","--es"])
+        assert os.path.isfile("out/conditionals.json")
+        assert os.path.isfile("out/filter-examples-badges.json")
+        assert os.path.isfile("out/filter-examples-buttonize.json")
+        assert os.path.isfile("out/filter-examples-callouts.json")
+        assert os.path.isfile("out/filter-examples-include_code.json")
+        assert os.path.isfile("out/filter-examples-multicode_tabs.json")
+        assert os.path.isfile("out/filter-examples-patriots.json")
+        assert os.path.isfile("out/filter-examples-patriots2.json")
+        assert os.path.isfile("out/filter-examples-xrefs.json")
+        assert os.path.isfile("out/gfm-compat.json")
+        assert os.path.isfile("out/includes.json")
+        assert os.path.isfile("out/lists-and-codeblocks.json")
 
-    def test_elastic_search(self):
-        subprocess.check_call(["dactyl_build","--es","--pages","content/gfm-compat.md"])
+    def test_elastic_search_single_page(self):
+        subprocess.check_call(["dactyl_build","--es","--pages","content/gfm-compat.json"])
         assert os.path.isfile("out/gfm-compat.json")
 
     def test_generate_markdown(self):
@@ -117,4 +131,5 @@ if __name__ == '__main__':
         os.chdir("examples")
     elif os.path.split(os.getcwd())=="tests":
         os.chdir("../examples")
-    unittest.main()
+    #unittest.main()
+    TestDactyl().test_elastic_search_default()
