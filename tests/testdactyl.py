0b79e3cd7c7ad254aafa11d4845e3c8581ec3316
Kenneth Cromack <kenneth.cromack@gmail.com>
2018-01-31 17:26:19 -0800

    Adding additional elastic search test

--- tests/testdactyl.py
+++ tests/testdactyl.py
@@ -87,6 +87,18 @@ class TestDactyl(unittest.TestCase):
         assert subprocess.call(["dactyl_style_checker","-t","filterdemos"]) != 0
 
     #P2 tests defined below
+    def test_elastic_search_default(self):
+        subprocess.check_call(["dactyl_build","--es"])
+        assert os.path.isfile("out/includes.json")
+        assert os.path.isfile("out/conditionals.json")
+        assert os.path.isfile("out/lists-and-codeblocks.json")
+        assert os.path.isfile("out/gfm-compat.json")
+        assert os.path.isfile("out/filter-examples-callouts.json")
+        assert os.path.isfile("out/filter-examples-xrefs.json")
+        assert os.path.isfile("out/filter-examples-buttonize.json")
+        assert os.path.isfile("out/filter-examples-badges.json")
+        assert os.path.isfile("out/filter-examples-include_code.json")
+        assert os.path.isfile("out/filter-examples-multicode_tabs.json")
 
     def test_elastic_search_single_page(self):
         subprocess.check_call(["dactyl_build","--es","--pages","content/gfm-compat.md"])
