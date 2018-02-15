ea4922c5cae1ae95cfea99b9ff12139619b107a7
EternalKenshi <33845125+EternalKenshi@users.noreply.github.com>
2018-02-15 13:11:23 -0800

    Add dactyl_link_checker and dactyl_style_checker tests (#32)
    * Adding dactyl_link_checker and dactyl_style_checker tests
    
    * Adding additional elastic search test
    
    * Adding test documentation

--- tests/testdactyl.py
+++ tests/testdactyl.py
@@ -9,15 +9,20 @@ from distutils.dir_util import remove_tree
 from pathlib import Path
 
 class TestDactyl(unittest.TestCase):
+    #IMPORTANT:  Please run this script from the "examples" directory.
 
     def setUp(self):
-        #Before each test is run, remonve the existing files in out subdirectory.
+        #Before each test is run, remove the existing files in out subdirectory.
         try:
 	        remove_tree("out")
         except FileNotFoundError:
             print("No out/ dir to remove")
 
     #P1 tests defined below
+
+    def tearDown(self):
+        #After running each test, reset the working directory to "examples".
+        os.chdir("../examples")
 
     def test_list(self):
         subprocess.check_output(["dactyl_build","-l"])
@@ -67,9 +72,36 @@ class TestDactyl(unittest.TestCase):
         assert os.path.isfile("out/filter-examples-include_code.html")
         assert os.path.isfile("out/filter-examples-multicode_tabs.html")
 
-    #P2 tests defined below
+    def test_dactyl_link_checker(self):
+        #Assert that the link checker exits with exit code 0, meaning that all links were validated successfully.
+        assert subprocess.check_call(["dactyl_link_checker"]) == 0
 
-    def test_elastic_search(self):
+    def test_dactyl_link_checker_with_broken_links(self):
+        os.chdir("../tests")
+        assert subprocess.call(["dactyl_link_checker"]) != 0
+
+    def test_dactyl_style_checker(self):
+        assert subprocess.check_call(["dactyl_style_checker","-t","conditionals"]) == 0
+
+    def test_dactyl_style_checker_with_known_issues(self):
+        #Assert that the style checker exits with a non-zero exit code, meaning it has found a styling error.
+        assert subprocess.call(["dactyl_style_checker","-t","filterdemos"]) != 0
+
+    #P2 tests defined below
+    def test_elastic_search_default(self):
+        subprocess.check_call(["dactyl_build","--es"])
+        assert os.path.isfile("out/includes.json")
+        assert os.path.isfile("out/conditionals.json")
+        assert os.path.isfile("out/lists-and-codeblocks.json")
+        assert os.path.isfile("out/gfm-compat.json")
+        assert os.path.isfile("out/filter-examples-callouts.json")
+        assert os.path.isfile("out/filter-examples-xrefs.json")
+        assert os.path.isfile("out/filter-examples-buttonize.json")
+        assert os.path.isfile("out/filter-examples-badges.json")
+        assert os.path.isfile("out/filter-examples-include_code.json")
+        assert os.path.isfile("out/filter-examples-multicode_tabs.json")
+
+    def test_elastic_search_single_page(self):
         subprocess.check_call(["dactyl_build","--es","--pages","content/gfm-compat.md"])
         assert os.path.isfile("out/gfm-compat.json")
 
@@ -111,6 +143,7 @@ class TestDactyl(unittest.TestCase):
         assert os.path.isfile("out/filter-examples-badges.html")
         assert os.path.isfile("out/filter-examples-include_code.html")
         assert os.path.isfile("out/filter-examples-multicode_tabs.html")
+
 
 if __name__ == '__main__':
     if os.path.basename(os.getcwd())=="dactyl":
